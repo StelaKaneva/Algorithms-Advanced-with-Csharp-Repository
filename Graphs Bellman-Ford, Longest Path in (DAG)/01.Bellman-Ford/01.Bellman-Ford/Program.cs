f5b4434ada4b4a44b0cece7d095b3e04a0ea2ab5
Stela Kaneva <70379843+StelaKaneva@users.noreply.github.com>
2022-09-16 11:32:58 +0300

    Implement Bellman-Ford Algorithm

---
+++ Graphs Bellman-Ford, Longest Path in (DAG)/01.Bellman-Ford/01.Bellman-Ford/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Bellman_Ford
+{
+    public class Edge
+    {
+        public int From { get; set; }
+
+        public int To { get; set; }
+
+        public int Weight { get; set; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var nodes = int.Parse(Console.ReadLine());
+            var edges = int.Parse(Console.ReadLine());
+
+            var graph = new List<Edge>();
+
+            for (int i = 0; i < edges; i++)
+            {
+                var edgeData = Console.ReadLine()
+                                .Split()
+                                .Select(int.Parse)
+                                .ToArray();
+
+                graph.Add(new Edge
+                {
+                    From = edgeData[0],
+                    To = edgeData[1],
+                    Weight = edgeData[2]
+                });
+            }
+
+            var source = int.Parse(Console.ReadLine());
+            var destination = int.Parse(Console.ReadLine());
+
+            var distance = new double[nodes + 1];
+            Array.Fill(distance, double.PositiveInfinity);
+            distance[source] = 0;
+
+            var prev = new int[nodes + 1];
+            Array.Fill(prev, -1);
+
+            for (int i = 0; i < nodes - 1; i++)
+            {
+                var updated = false;
+
+                foreach (var edge in graph)
+                {
+                    if (double.IsPositiveInfinity(distance[edge.From])) // If the edge hasn't been visited so far
+                    {
+                        continue;
+                    }
+
+                    var newDistance = distance[edge.From] + edge.Weight;
+
+                    if (newDistance < distance[edge.To])
+                    {
+                        distance[edge.To] = newDistance;
+                        prev[edge.To] = edge.From;
+                        updated = true;
+                    }
+                }
+
+                if (!updated)
+                {
+                    break;
+                }
+            }
+
+            foreach (var edge in graph)
+            {
+                var newDistance = distance[edge.From] + edge.Weight;
+
+                if (newDistance < distance[edge.To])
+                {
+                    Console.WriteLine("Negative Cycle Detected");
+                    return;
+                }
+            }
+
+            var path = new Stack<int>();
+            var node = destination;
+
+            while (node != -1)
+            {
+                path.Push(node);
+                node = prev[node];
+            }
+
+            Console.WriteLine(string.Join(" ", path));
+
+            Console.WriteLine(distance[destination]);
+        }
+    }
+}
